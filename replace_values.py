0b2565f23d2384214b1e1adb7ffa50ce42c19d69
Johan Lehto <93596859+SvartKaffe@users.noreply.github.com>
2021-12-17 10:54:13 +0100

    added comments

--- replace_values.py
+++ replace_values.py
@@ -5,9 +5,19 @@ from shutil import copyfile
 
 def replace_values(sample_no, sample_vol, ratio, clean_amt, eb_vol):
     """
-    Takes 5 parameters which are samples, volume, ratio, washes and volumeEB. It reads from a file which acts
-    as a blueprint to write in an other file. Uses the input parameters and replaces the blueprint lines with
-    those instead, and creates the final protocol file.
+    Takes 5 parameters which are samples, volume, ratio, number of washes and volumeEB. It reads from a file which acts
+    as a blueprint to write in another file. Appends the parameters at the bottom of the blueprint and creates the final 
+    protocol file.
+
+        Parameters:
+            sample_no (int):            Total number of samples
+            samepl_vol (int):           Sample volume
+            ratio (float):              Samlpe/SPRI-bead ratio
+            clean_amt (int):            Number of cleanings with ethanol
+            eb_vol (int):               Volume of elution buffer to be used
+        
+            Returns:
+                Nothing.
     """
 
     local_user = os.getlogin()
